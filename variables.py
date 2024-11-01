d23c3bc54f743ca1373c7c6b6213aa17d6c2832d
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-11-01 13:35:36 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+num_of_completed_HW = 12
+spend_time = 1.5
+course_name = 'Python'
+time_to_single_task = spend_time/num_of_completed_HW
+print(f'Курс: {course_name}, всего задач: {num_of_completed_HW}, затрачено часов: {spend_time}, среднее время выполнения {time_to_single_task} часа.')
